ce9a3b17de4f4fe83bcf1387bb4df0833357d904
kimts2627 <kimtstststs@gmail.com>
2021-04-23 14:19:36 +0900

    tui editor
    <Footer>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import {useState} from 'react';
 
-function App() {
+import { Editor } from '@toast-ui/react-editor';
+
+const App = () => {
+
+  const [data, setData] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Editor 
+      previewStyle="vertical"
+      height="700px"
+      initialEditType="markdown"
+      initialValue="hello"
+      usageStatistics={false}
+    />
   );
 }
 
